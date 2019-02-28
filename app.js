084324bcda1801566f9d04677c949c1939e7f955
tundeUsman <tundeusman0@gmail.com>
2019-02-28 13:12:51 +0100

    installed module packages and setup app.js [#164300759]

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log("connected to port 3000");
+});
