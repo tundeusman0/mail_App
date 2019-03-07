5591c9da81d10a4ef7ef280692190232657d3205
tundeUsman <tundeusman0@gmail.com>
2019-03-07 13:17:20 +0100

    added user sign-in API route [#164302915]

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 const utils = require('./../utils/utils');
 
-
+// POST signup 
 router.post('/api/v1/auth/signup', (req, res) => {
     const {
  id, email, password, firstName, lastName, token,
 } = req.body;
     const user = {
-        id, email, password, firstName, lastName,
+        id, email, password, firstName, lastName, token,
     };
     utils.addUser(user).then(() => {
         res.status(200).send({
@@ -25,5 +25,29 @@ router.post('/api/v1/auth/signup', (req, res) => {
         });
     });
 });
+
+// POST login
+router.post('/api/v1/auth/login', (req, res) => {
+    let { email, password } = req.body
+    let login = {
+        email, password
+    }
+
+    utils.getUser(login).then(user => {
+        let token = user[0].token
+        res.status(200).send({
+            status: 200,
+            data: [{
+                token
+            }]
+        })
+    }).catch(err => {
+        res.status(400).send({
+            status: 400,
+            error: "relevant-error-message"
+        })
+    })
+
+})
 
 module.exports = router;
