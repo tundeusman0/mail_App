94d5dce17bc3e31492534f8c264f103fdfb762da
tundeUsman <tundeusman0@gmail.com>
2019-03-07 12:59:28 +0100

    added user sign up route [#164302906]

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const route = require('./routes/api_routes');
 
+// init App
 const app = express();
+
+// body Parser middleware
 app.use(bodyParser.json());
+
+app.use('/', route);
 
 app.listen(3000, () => {
     console.log("connected to port 3000");
