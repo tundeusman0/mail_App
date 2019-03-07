9928241a7247c69dd12523c753f4474fba51b336
tundeUsman <tundeusman0@gmail.com>
2019-03-07 18:47:22 +0100

    chore:Test POST login [#164467947]

--- tests/app.test.js
+++ tests/app.test.js
@@ -41,3 +41,39 @@ const { password } = users[0];
             });
     });
 });
+
+// test POST login
+describe('POST /api/v1/auth/login', () => {
+    // test POST login which should pass user login
+    it('should login user', (done) => {
+        const { email } = users[1];
+const { password } = users[1];
+        request(app)
+            .post('/api/v1/auth/login')
+            .send({ email, password })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.data[0].token).toBe(users[1].token);
+            })
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            });
+    });
+    // test POST login which user should not login with different email and password
+    it('should reject invalid login', (done) => {
+        const { email } = users[1];
+const { password } = users[0];
+        request(app)
+            .post('/api/v1/auth/login')
+            .send({ email, password })
+            .expect(400)
+            .expect((res) => {
+                expect(res.body.error).toEqual("No User exist");
+            })
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            });
+    });
+});
