cfe14c1891b80c9a407b20ba37f4c30fb10f3870
tundeUsman <tundeusman0@gmail.com>
2019-03-07 14:06:17 +0100

    added messages POST API router [#164461985]

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -50,4 +50,27 @@ router.post('/api/v1/auth/login', (req, res) => {
 
 })
 
+// POST messages
+router.post('/api/v1/messages', (req, res) => {
+    const { subject, message, id, createdOn } = req.body
+    const status = "read";
+    const parentMessageId = 105;
+    const messag = {
+        id, createdOn, subject, message, parentMessageId, status
+    }
+    utils.createMessage(messag).then(message => {
+        res.status(200).send({
+            status: 200,
+            data: [{
+                ...message
+            }]
+        })
+    }).catch(err => {
+        res.status(400).send({
+            status: 400,
+            error: "relevant-error-message"
+        })
+    })
+
+})
 module.exports = router;
