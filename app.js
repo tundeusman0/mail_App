07952d3df314767890a8b23fd935074e8356854e
tundeUsman <tundeusman0@gmail.com>
2019-03-08 11:10:55 +0100

    chore: Setup app for heroku[#164491154]

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const route = require('./routes/api_routes');
 // init App
 const app = express();
 
+// heroku Port
+const port = process.env.PORT || 3000
+
 // body Parser middleware
 app.use(bodyParser.json());
 
 app.use('/', route);
 
-app.listen(3000, () => {
-    console.log("connected to port 3000");
+app.listen(port, () => {
+    console.log(`Strated up at port ${port}`);
 });
 
 module.exports = app;
